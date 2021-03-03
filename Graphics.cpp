81b3e6ede46430e453e1c6d6a7c726ef432e25c2
Jey Bool <jeymuthuraj109245@gmail.com>
2021-03-02 23:15:24 -0500

    no more smart ptr for graphics

--- Graphics.cpp
+++ Graphics.cpp
@@ -6,7 +6,7 @@ void drawFrame(unsigned char display[][DISPLAY_COLS], sf::RenderWindow& window, 
 
 	//flag 1 means draw, flag 2 means clear screen, which is done above, so just skip and display
 	if (flag == 1) {
-		std::unique_ptr<sf::RectangleShape> rec_ptr(new sf::RectangleShape(sf::Vector2f(SCALE_FACTOR, SCALE_FACTOR)));
+		sf::RectangleShape* rec_ptr = new sf::RectangleShape(sf::Vector2f(SCALE_FACTOR, SCALE_FACTOR));
 		//the bounds for this loop(and size of rectangle) should be scaled relative to each other, maybe use some constants for
 		//the height/width of window, and determine the dims of the rectangle using that
 		for (int y = 0; y < DISPLAY_ROWS * SCALE_FACTOR; y += SCALE_FACTOR) {
@@ -24,6 +24,8 @@ void drawFrame(unsigned char display[][DISPLAY_COLS], sf::RenderWindow& window, 
 				window.draw(*rec_ptr);
 			}
 		}
+
+		delete rec_ptr;
 	}
 
 	window.display();
